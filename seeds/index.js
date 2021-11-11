e5911bf62fda9df802beea07c416e5074524205f
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-11-11 13:59:41 -0500

    working and updating the linking seeds file

--- seeds/index.js
+++ seeds/index.js
@@ -1,26 +1,24 @@
 const seedCategories = require('./category-seeds');
-const seedProducts = require('./product-seeds');
-const seedTags = require('./tag-seeds');
-const seedProductTags = require('./product-tag-seeds');
+const seedProducts = require('./product-seeds')
+const seedTags = require('./tag-seeds')
+const seedProductTags = require('./product-tag-seeds')
 
 const sequelize = require('../config/connection');
 
 const seedAll = async () => {
-  await sequelize.sync({ force: true });
-  console.log('\n----- DATABASE SYNCED -----\n');
-  await seedCategories();
-  console.log('\n----- CATEGORIES SEEDED -----\n');
+    await sequelize.sync({ force: true});
+    console.log(' database synced');
+    await seedCategories();
+    console.log('catgeories seeded')
+    await seedProducts();
+    console.log('products seeded');
+    await seedTags();
+    console.log('tags seeded');
+    await seedProductTags();
+    console.log('seeded')
 
-  await seedProducts();
-  console.log('\n----- PRODUCTS SEEDED -----\n');
-
-  await seedTags();
-  console.log('\n----- TAGS SEEDED -----\n');
-
-  await seedProductTags();
-  console.log('\n----- PRODUCT TAGS SEEDED -----\n');
-
-  process.exit(0);
-};
+    process.exit(0);
+}
 
 seedAll();
+
